04d74f441a3bdc442427da1e907def6c7529015f
Cristian González <kazragore96@gmail.com>
2021-06-17 17:39:42 -0500

    Next script en diferido

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,5 +1,22 @@
 import React from "react";
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript as OriginalNextScript,
+} from "next/document";
+
+class NextScript extends OriginalNextScript {
+  getScripts() {
+    return super.getScripts().map((script) => {
+      return React.cloneElement(script, {
+        key: script.props.src,
+        defer: true,
+        async: false,
+      });
+    });
+  }
+}
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
