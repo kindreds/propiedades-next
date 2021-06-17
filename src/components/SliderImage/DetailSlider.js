d9768f462090380efaf345093ea08ed479523bd7
Cristian González <kazragore96@gmail.com>
2021-06-17 18:51:54 -0500

    Slider cargado en el server

---
+++ src/components/SliderImage/DetailSlider.js
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import Image from "next/image";
+import Icon from "@chakra-ui/icon";
+import ReactSlider from "react-slidy";
+import { Box } from "@chakra-ui/layout";
+import { IconButton } from "@chakra-ui/button";
+import { useBreakpointValue } from "@chakra-ui/media-query";
+
+import { GoPrimitiveDot } from "react-icons/go";
+
+const initialState = {
+  base: 1,
+  ms: 1,
+  sm: 2,
+  md: 2,
+  lg: 2,
+  xl: 3,
+};
+
+const DetailSlider = () => {
+  const [actualSlide, setActualSlide] = useState(0);
+  const numOfSlidesRaw = useBreakpointValue(initialState);
+
+  const updateSlide = ({ currentSlide }) => {
+    setActualSlide(currentSlide);
+  };
+
+  const numOfSlides = numOfSlidesRaw ?? 3;
+
+  const imagesList = [
+    "/inhouse_1_cp.webp",
+    "/inhouse_2_cp.webp",
+    "/inhouse_3_cp.webp",
+    "/inhouse_4_cp.webp",
+    "/inhouse_5_cp.webp",
+    "/inhouse_6_cp.webp",
+  ];
+
+  return (
+    <>
+      <ReactSlider
+        showArrows
+        infiniteLoop
+        keyboardNavigation
+        slide={actualSlide}
+        numOfSlides={numOfSlides}
+        doAfterSlide={updateSlide}
+      >
+        {imagesList.map((src, i) => (
+          <Image
+            key={i}
+            src={src}
+            width="300"
+            height="200"
+            layout="responsive"
+            objectPosition="50% 50%"
+          />
+        ))}
+      </ReactSlider>
+      <Box
+        pt={2}
+        pb={5}
+        w="full"
+        display="flex"
+        justifyContent="center"
+        alignItems="flex-start"
+      >
+        {imagesList.map((_, index) => {
+          return (
+            <IconButton
+              key={index}
+              rounded="full"
+              variant="unstyled"
+              sx={{ outline: "none", border: "none" }}
+              opacity={index === actualSlide ? 1 : 0.3}
+              icon={<Icon as={GoPrimitiveDot} fontSize="25px" mt="auto" />}
+              onClick={() => updateSlide({ currentSlide: index })}
+            />
+          );
+        })}
+      </Box>
+    </>
+  );
+};
+
+export default DetailSlider;
--- src/pages/propiedades/[slug].js
+++ src/pages/propiedades/[slug].js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
+
 import dynamic from "next/dynamic";
-import Image from "next/image";
-import ReactSlider from "react-slidy";
+
 import {
   Box,
   Stack,
@@ -12,7 +12,6 @@ import {
 } from "@chakra-ui/layout";
 import { Tooltip } from "@chakra-ui/tooltip";
 import { IconButton } from "@chakra-ui/button";
-// import { Img as ImageChakra } from "@chakra-ui/image";
 
 import {
   FaTwitter,
@@ -20,15 +19,11 @@ import {
   FaFacebookF,
   FaLinkedinIn,
 } from "react-icons/fa";
-import Icon from "@chakra-ui/icon";
 import { BsChatFill } from "react-icons/bs";
-import { GoPrimitiveDot } from "react-icons/go";
 import { RiWhatsappFill } from "react-icons/ri";
-import { BiPrinter, BiShareAlt } from "react-icons/bi";
-
 import { useDisclosure } from "@chakra-ui/hooks";
+import { BiPrinter, BiShareAlt } from "react-icons/bi";
 import { useInView } from "react-intersection-observer";
-import { useBreakpointValue } from "@chakra-ui/media-query";
 
 import AsesorCard from "../../components/AsesorCard";
 import PropertiHeader from "../../components/PropertiHeader";
@@ -54,85 +49,28 @@ const Contact = dynamic(() => import("../../components/Contact"), {
 const Map = dynamic(() => import("../../components/Map"), {
   ssr: false,
 });
+const DetailSlider = dynamic(
+  () => import("../../components/SliderImage/DetailSlider"),
+  {
+    ssr: true,
+    loading: () => <p>Loading...</p>,
+  }
+);
 
 const Propiedad = () => {
-  const [actualSlide, setActualSlide] = useState(0);
   const { isOpen, onClose, onOpen } = useDisclosure();
   const { ref: socialRef, inView: SocialInView } = useInView();
-  const numOfSlidesRaw = useBreakpointValue({
-    base: 1,
-    ms: 1,
-    sm: 2,
-    md: 2,
-    lg: 2,
-    xl: 3,
-  });
-
-  const updateSlide = ({ currentSlide }) => {
-    setActualSlide(currentSlide);
-  };
 
   const ScrollToContact = () => {
     const el = document.getElementById("asesor_msg");
     el.scrollIntoView({ behavior: "smooth", block: "start" });
   };
 
-  const numOfSlides = numOfSlidesRaw ?? 3;
-
-  const imagesList = [
-    "/inhouse_1_cp.webp",
-    "/inhouse_2_cp.webp",
-    "/inhouse_3_cp.webp",
-    "/inhouse_4_cp.webp",
-    "/inhouse_5_cp.webp",
-    "/inhouse_6_cp.webp",
-  ];
-
   return (
     <>
       <Box bg="gray.200" w="100%">
         <Box w="full">
-          <ReactSlider
-            showArrows
-            infiniteLoop
-            keyboardNavigation
-            slide={actualSlide}
-            numOfSlides={numOfSlides}
-            doAfterSlide={updateSlide}
-          >
-            {imagesList.map((src, i) => (
-              <Image
-                key={i}
-                src={src}
-                width="300"
-                height="200"
-                layout="responsive"
-                objectPosition="50% 50%"
-              />
-            ))}
-          </ReactSlider>
-          <Box
-            pt={2}
-            pb={5}
-            w="full"
-            display="flex"
-            justifyContent="center"
-            alignItems="flex-start"
-          >
-            {imagesList.map((_, index) => {
-              return (
-                <IconButton
-                  key={index}
-                  rounded="full"
-                  variant="unstyled"
-                  sx={{ outline: "none", border: "none" }}
-                  opacity={index === actualSlide ? 1 : 0.3}
-                  icon={<Icon as={GoPrimitiveDot} fontSize="25px" mt="auto" />}
-                  onClick={() => updateSlide({ currentSlide: index })}
-                />
-              );
-            })}
-          </Box>
+          <DetailSlider />
         </Box>
 
         <Container
