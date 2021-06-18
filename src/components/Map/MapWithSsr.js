eee3359156689f1dcbbbafc6c763bcd966baba53
Cristian González <kazragore96@gmail.com>
2021-06-18 10:24:26 -0500

    Bug de mapa corregido

---
+++ src/components/Map/MapWithSsr.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
+import "leaflet/dist/leaflet.css";
+import "leaflet-defaulticon-compatibility";
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
+
+const MapWithSsr = () => {
+  return (
+    <MapContainer
+      zoom={16}
+      scrollWheelZoom={false}
+      center={[-12.115194, -77.013199]}
+      style={{ height: "100%", width: "100%", zIndex: 1 }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker
+        position={[-12.115194, -77.013199]}
+        draggable={true}
+        animate={true}
+      >
+        <Popup>Hey ! I live here</Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default MapWithSsr;
--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,12 +1,10 @@
 import React, { useEffect, useState } from "react";
+import d from "next/dynamic";
 import NextLink from "next/link";
 import { useInView } from "react-intersection-observer";
 import { Box, Heading, Link, Text } from "@chakra-ui/layout";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
-import "leaflet/dist/leaflet.css";
-import "leaflet-defaulticon-compatibility";
-import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
+const MapWithSsr = d(() => import("./MapWithSsr"), { ssr: false });
 
 const Map = () => {
   const { ref, inView } = useInView();
@@ -37,28 +35,9 @@ const Map = () => {
           </NextLink>
         </Box>
 
-        {load && (
-          <Box h="400px" id="mapid">
-            <MapContainer
-              zoom={16}
-              scrollWheelZoom={false}
-              center={[-12.115194, -77.013199]}
-              style={{ height: "100%", width: "100%", zIndex: 1 }}
-            >
-              <TileLayer
-                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-              />
-              <Marker
-                position={[-12.115194, -77.013199]}
-                draggable={true}
-                animate={true}
-              >
-                <Popup>Hey ! I live here</Popup>
-              </Marker>
-            </MapContainer>
-          </Box>
-        )}
+        <Box h="400px" id="mapid">
+          {load && <MapWithSsr />}
+        </Box>
       </Box>
     </>
   );
